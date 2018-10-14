b2ffc9fe886322454da45130b30ae80758a88ae5
Fahade <Vous@exemple.com>
2018-10-14 20:39:57 +0200

    commit for deploy to heroku

--- main.go
+++ main.go
@@ -1,87 +1,168 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
-  "fmt"
-  "net/http"
-  "os"
-  //"strings"
-  "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+
+	igc "github.com/marni/goigc"
 )
 
-type InfoApi struct {
-    Uptime time.Time `json:"uptime,omitempty"`
-    Info string `json:"info,omitempty"`
-    Version string `json:"version,omitempty"`
+type igcTrack struct {
+	H_date       string  //"H_date": <date from File Header, H-record>,
+	Pilot        string  //"pilot": <pilot>,
+	Glider       string  //"glider": <glider>,
+	Glider_id    string  //"glider_id": <glider_id>,
+	Track_length float64 //"track_length": <calculated total track length>
 }
 
+type API struct {
+	Uptime  string //"uptime": <uptime>
+	Info    string //"info": "Service for IGC tracks."
+	Version string //"version": "v1"
 
-
-func determineListenAddress() (string, error) {
-  port := os.Getenv("PORT")
-  if port == "" {
-    return "", fmt.Errorf("$PORT not set")
-  }
-  return ":" + port, nil
+}
+type igcFile struct {
+	Url string //a valid igc URL
 }
 
+type igcDB struct {
+	igcs map[string]igcFile
+}
 
+func (db *igcDB) add(igc igcFile, id string) {
+	for _, file := range db.igcs {
+		if igc == file {
+			return
+		}
+	}
+	db.igcs[id] = igc
+}
 
-func initApi(w http.ResponseWriter, r *http.Request) {
-  	http.Header.Add(w.Header(),"content-type","application/json")
+func (db igcDB) Count() int {
+	return len(db.igcs)
+}
 
-  	infoApi:=InfoApi {
-    		 Uptime: time.Now(),
-    		 Info: "Service for IGC tracks.",
-    		 Version: "v1",
-  	}
-	
-	json.NewEncoder(w).Encode(infoApi)
-
-	/*var infoApi2 InfoApi
-   	err := json.NewDecoder(r.Body).Decode(&infoApi2)
-   	if err != nil {
-      		log.Fatal(err)
-   	}
-
-   	fmt.Println(infoApi)*/
+func (db igcDB) Get(idWanted string) igcFile {
+	for id, file := range db.igcs {
+		if idWanted == id {
+			return file
+		}
+	}
+	return igcFile{}
 }
 
 func getApi(w http.ResponseWriter, r *http.Request) {
-	
+	http.Header.Add(w.Header(), "content-type", "application/json")
+	//io.WriteString(w, "Api information :\n")
+	api := &API{}
+	api.Uptime = "uptime"
+	api.Info = "Service for IGC tracks."
+	api.Version = "version : v1"
+	//fmt.Fprintf(w, "%s\n%s\n%s", api.Uptime, api.Info, api.Version)
+	json.NewEncoder(w).Encode(api)
 
-   	var infoApi InfoApi
-   	err := json.NewDecoder(r.Body).Decode(&infoApi)
-   	if err != nil {
-      		log.Fatal(err)
-   	}
-
-   	fmt.Println(infoApi)
 }
 
-func postIgc(w http.ResponseWriter, r *http.Request) {
-  	http.Header.Add(w.Header(),"content-type","application/json")
+func igcHandler(w http.ResponseWriter, r *http.Request) {
 
-  	url:="http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
-	json.NewEncoder(w).Encode(url)
+	switch r.Method {
+	case "POST":
+		{
+
+			if r.Body == nil {
+				http.Error(w, "no JSON body", http.StatusBadRequest)
+				return
+			}
+			var igc igcFile
+			err := json.NewDecoder(r.Body).Decode(&igc)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			//TODO check correct igc URL
+			fmt.Fprintf(w, "URL : %s\n", igc.Url)
+			Idstr := "id"
+			strValue := fmt.Sprintf("%d", idCount)
+			newId := Idstr + strValue
+			ids = append(ids, newId)
+			idCount += 1
+			db.add(igc, newId)
+			json.NewEncoder(w).Encode(newId)
+		}
+	case "GET":
+		{
+			//GET case
+			http.Header.Add(w.Header(), "content-type", "application/json")
+			parts := strings.Split(r.URL.Path, "/")
+			//fmt.Fprintf(w, "longueur : %d\n", len(parts))
+			//fmt.Fprintln(w, parts)
+			if len(parts) < 5 || len(parts) > 6 {
+				//deal with errors
+				fmt.Fprintln(w, "wrong numbers of parameters")
+				return
+			}
+			if parts[4] == "" {
+				//deal with the array
+				fmt.Fprintln(w, "Display the array")
+				json.NewEncoder(w).Encode(ids)
+
+			}
+			if parts[4] != "" {
+				fmt.Fprintln(w, "Information about the id")
+				//deal with the id
+				var igcWanted igcFile
+				rgx, _ := regexp.Compile("^id[0-9]*")
+				id := parts[4]
+				if rgx.MatchString(id) == true {
+					igcWanted = db.Get(id)
+
+					//then encode the igcFile
+					url := igcWanted.Url
+					track, err := igc.ParseLocation(url)
+					if err != nil {
+						//fmt.Errorf("Problem reading the track", err)
+					}
+					igcT := igcTrack{}
+					igcT.Glider = track.GliderType
+					igcT.Glider_id = track.GliderID
+					igcT.Pilot = track.Pilot
+					igcT.Track_length = track.Task.Distance()
+					igcT.H_date = track.Date.String()
+					json.NewEncoder(w).Encode(igcT)
+				}
+				if rgx.MatchString(id) == false {
+					fmt.Fprintln(w, "Use format id0 or id21 for exemple")
+				}
+			}
+			/*if parts[5]  {
+				fmt.Fprintln(w, "case field")
+				/*infoWanted := parts[5]
+				id := parts[4]
+
+			}*/
+
+		}
+	default:
+
+		http.Error(w, "not implemented yet", http.StatusNotImplemented)
+
+	}
 }
 
+var db igcDB
+var ids []string
+var idCount int
 
 func main() {
-  	addr, err := determineListenAddress()
-  	if err != nil {
-    		log.Fatal(err)
-  	}
-//Api_Root := "https://glacial-wave-53134.herokuapp.com"
-
-  	http.HandleFunc("/api", initApi)
-  	//http.HandleFunc("/api/igc", postIgc)
-	/*resp,err := http.Post(addr+"/api/igc","application/json",strings.NewReader("{\"url\": http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc}"))
-	if err != nil {
-      		log.Fatal(err)
-   	}*/
-  	log.Fatal(http.ListenAndServe(addr,nil))
-  	
+	db = igcDB{}
+	db.igcs = map[string]igcFile{}
+	idCount = 0
+	ids = nil
+	port := os.Getenv("PORT")
+	http.HandleFunc("/igcinfo/api", getApi)
+	http.HandleFunc("/igcinfo/api/igc/", igcHandler)
+	http.ListenAndServe(":"+port, nil)
 }
-
