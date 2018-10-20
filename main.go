9e0fc17cea349e14687f6adb2fc8fcffbbfb6974
Fahade <Vous@exemple.com>
2018-10-20 17:26:15 +0200

    commit for deploy to heroku

--- main.go
+++ main.go
@@ -1,32 +1,83 @@
 package main
- 
- import (
- 	"fmt"
- 	"log"
- 	"net/http"
- 	"os"
- )
- 
- func main() {
- 	http.HandleFunc("/", handler)
- 	fmt.Println("listening...")
- 	err := http.ListenAndServe(GetPort(), nil)
- 	if err != nil {
- 		log.Fatal("ListenAndServe: ", err)
- 	}
- }
- 
- func handler(w http.ResponseWriter, r *http.Request) {
- 	fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
- }
- 
- // Get the Port from the environment so we can run on Heroku
- func GetPort() string {
- 	var port = os.Getenv("PORT")
- 	// Set a default port if there is nothing in the environment
- 	if port == "" {
- 		port = "4747"
- 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
- 	}
- 	return ":" + port
- }
+
+import (
+  "encoding/json"
+  "log"
+  "fmt"
+  "net/http"
+  "os"
+  "time"
+)
+
+type InfoApi struct {
+    Uptime time.Time `json:"uptime,omitempty"`
+    Info string `json:"info,omitempty"`
+    Version string `json:"version,omitempty"`
+}
+
+
+
+func determineListenAddress() (string, error) {
+  port := os.Getenv("PORT")
+  if port == "" {
+    return "", fmt.Errorf("$PORT not set")
+  }
+  return ":" + port, nil
+}
+
+
+
+func initApi(w http.ResponseWriter, r *http.Request) {
+  	http.Header.Add(w.Header(),"content-type","application/json")
+
+  	infoApi:=InfoApi {
+    		 Uptime: time.Now(),
+    		 Info: "Service for IGC tracks.",
+    		 Version: "v1",
+  	}
+	
+	json.NewEncoder(w).Encode(infoApi)
+
+	/*var infoApi2 InfoApi
+   	err := json.NewDecoder(r.Body).Decode(&infoApi2)
+   	if err != nil {
+      		log.Fatal(err)
+   	}
+
+   	fmt.Println(infoApi)*/
+}
+
+func getApi(w http.ResponseWriter, r *http.Request) {
+	
+
+   	var infoApi InfoApi
+   	err := json.NewDecoder(r.Body).Decode(&infoApi)
+   	if err != nil {
+      		log.Fatal(err)
+   	}
+
+   	fmt.Println(infoApi)
+}
+
+func postIgc(w http.ResponseWriter, r *http.Request) {
+  	http.Header.Add(w.Header(),"content-type","application/json")
+
+  	url:="http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	json.NewEncoder(w).Encode(url)
+}
+
+
+func main() {
+  	addr, err := determineListenAddress()
+  	if err != nil {
+    		log.Fatal(err)
+  	}
+
+
+  	http.HandleFunc("/api", initApi)
+  	http.HandleFunc("/api/igc", postIgc)
+	
+  	log.Fatal(http.ListenAndServe(addr,nil))
+  	
+}
+
